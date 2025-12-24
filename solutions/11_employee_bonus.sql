bd0dbe7127fffea581ee1481526d86cc6d28d20e
IbtsamGhreeb <ibrahimibtsam82@gmail.com>
2025-12-24 12:26:59 +0200

    Create 11_employee_bonus.sql

---
+++ solutions/11_employee_bonus.sql
@@ -0,0 +1,8 @@
+-- 577. Employee Bonus
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/employee-bonus/
+
+/* Write your T-SQL query statement below */
+select product_id
+from products
+where low_fats='Y' and recyclable='Y'
