1d8e3362f13384f37bf84498ce0e13553fcad78d
IbtsamGhreeb <ibrahimibtsam82@gmail.com>
2026-01-07 00:44:14 +0200

    Create 29_Customers_Who_Bought_All_Products.sql

---
+++ solutions/29_Customers_Who_Bought_All_Products.sql
@@ -0,0 +1,11 @@
+-- 1045. Customers Who Bought All Products
+-- Difficulty: Medium
+-- Link: https://leetcode.com/problems/Customers-Who-Bought-All-Products
+
+select c.customer_id
+from Customer c join Product p
+on c.product_key=p.product_key
+group by c.customer_id
+having count(distinct c.product_key)=(
+    select count(product_key)
+    from Product)
