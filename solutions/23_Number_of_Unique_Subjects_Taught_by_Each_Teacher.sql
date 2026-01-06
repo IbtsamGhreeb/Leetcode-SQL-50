806ffadbb7d6dc6a6922484ba353c19be3e7befe
IbtsamGhreeb <ibrahimibtsam82@gmail.com>
2026-01-07 00:30:01 +0200

    Create 23_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql

---
+++ solutions/23_Number_of_Unique_Subjects_Taught_by_Each_Teacher.sql
@@ -0,0 +1,8 @@
+-- 2356. Number of Unique Subjects Taught by Each Teacher
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/Number-of-Unique-Subjects-Taught-by-Each-Teacher
+
+# Write your MySQL query statement below
+select teacher_id,count(distinct subject_id) as cnt
+from Teacher
+group by teacher_id
