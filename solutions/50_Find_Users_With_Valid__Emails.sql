b9d7f7bb0233692071147799aa0359ade8569872
IbtsamGhreeb <ibrahimibtsam82@gmail.com>
2026-01-07 01:28:55 +0200

    Create 50_Find_Users_With_Valid__Emails.sql

---
+++ solutions/50_Find_Users_With_Valid__Emails.sql
@@ -0,0 +1,10 @@
+-- 1517. Find Users With Valid E-Mails
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/Find-Users-With-Valid-E-Mails
+
+
+# Write your MySQL query statement below
+SELECT user_id,name,mail                  
+FROM Users 
+WHERE mail REGEXP'^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' AND mail LIKE BINARY '%@leetcode.com';
+
